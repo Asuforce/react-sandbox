ffb927c62bedfb02661ece3e4bfb55508c3952c0
asuforce <owata.sn@gmail.com>
2018-11-01 16:02:54 +0900

    Export params

--- src/actions.ts
+++ src/actions.ts
@@ -1,7 +1,7 @@
 // action types
-const ADD_TODO = "ADD_TODO";
-const TOGGLE_TODO = "TOGGLE_TODO";
-const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
 
 // other constants
 export const VisibilityFilters = {
