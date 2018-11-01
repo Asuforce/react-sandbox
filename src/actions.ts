c3ca47a03b05b23ad52c70529d88d66427874aaf
asuforce <owata.sn@gmail.com>
2018-11-01 15:15:09 +0900

    Create actions
    FYI: https://redux.js.org/basics/actions

---
+++ src/actions.ts
@@ -0,0 +1,33 @@
+// action types
+const ADD_TODO = "ADD_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
+
+// other constants
+export const VisibilityFilters = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVATE: "SHOW_ACTIVATE"
+};
+
+// action creators
+function addTodo(text: string) {
+  return {
+    type: ADD_TODO,
+    text
+  };
+}
+
+function toggleTodo(index: number) {
+  return {
+    type: TOGGLE_TODO,
+    index
+  };
+}
+
+function setVisibilityFilters(filter) {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter
+  };
+}
