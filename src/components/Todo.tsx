757938f8b6ab047ba22a0c69fbab3612294ce818
asuforce <owata.sn@gmail.com>
2018-11-02 17:41:16 +0900

    Create Todo

---
+++ src/components/Todo.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import * as PropTypes from "prop-types";
+
+const Todo = ({
+  onClick,
+  completed,
+  text
+}: {
+  onClick: any;
+  completed: boolean;
+  text: string;
+}) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? "line-through" : "none"
+    }}
+  >
+    {text}
+  </li>
+);
+
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired
+};
+
+export default Todo;
