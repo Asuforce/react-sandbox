51b5cf9221fe13379eb28fddcc864778cac67bc2
asuforce <owata.sn@gmail.com>
2018-11-02 17:50:32 +0900

    Create TodoList

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import Todo from "./Todo";
+
+const TodoList = ({ todos, onTodoClick }: { todos: any; onTodoClick: any }) => (
+  <ul>
+    {todos.map((todo: any, index: number) => (
+      <Todo key={index} {...todo} onClick={() => onTodoClick(index)} />
+    ))}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  onTodoClick: PropTypes.func.isRequired
+};
+
+export default TodoList;
