8507942b5cab51d07f70f4cc6f4ced5d0c529263
asuforce <owata.sn@gmail.com>
2018-11-05 13:29:43 +0900

    Fix typo

--- src/actions.ts
+++ src/actions.ts
@@ -7,7 +7,7 @@ export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
 export const VisibilityFilters = {
   SHOW_ALL: "SHOW_ALL",
   SHOW_COMPLETED: "SHOW_COMPLETED",
-  SHOW_ACTIVATE: "SHOW_ACTIVATE"
+  SHOW_ACTIVE: "SHOW_ACTIVE"
 };
 
 // action creators
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,11 @@ import { VisibilityFilters } from "actions";
 const Footer = () => (
   <p>
     Show: <FilterLink filter={VisibilityFilters.SHOW_ALL}>ALL</FilterLink>
-    {','}
+    {","}
     <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
-    {','}
+    {","}
     <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
   </p>
-)
+);
 
 export default Footer;
