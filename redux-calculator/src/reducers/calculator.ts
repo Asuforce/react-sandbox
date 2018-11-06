d1cdb41783b471f9ba52b1a5e4745c39838cb563
asuforce <owata.sn@gmail.com>
2018-11-06 19:29:14 +0900

    Define interface and fix some bugs
    FYI: https://twitter.com/asuforce/status/1059745898236768256

--- redux-calculator/src/reducers/calculator.ts
+++ redux-calculator/src/reducers/calculator.ts
@@ -1,33 +1,34 @@
-import * as actionTypes from '../utils/actionTypes';
+import * as actionTypes from 'utils/actionTypes';
 
-const initialAppState = {
+export interface Calculator {
+  inputValue: number;
+  resultValue: number;
+  showingResult: boolean;
+}
+
+const initialAppState: Calculator = {
   inputValue: 0,
   resultValue: 0,
   showingResult: false,
 };
 
 export default (
-  state: {
-    inputValue: number;
-    resultValue: number;
-    showingResult: boolean;
-  } = initialAppState,
-  action: {
-    type: string;
-  },
+  state: Calculator = initialAppState,
+  action: { type: string; num: number },
 ) => {
   switch (action.type) {
     case actionTypes.INPUT_NUMBER:
       return {
         ...state,
-        inputValue: state.inputValue * 10 + actionTypes.INPUT_NUMBER,
+        inputValue: state.inputValue * 10 + action.num,
         showingResult: false,
       };
     case actionTypes.PLUS:
       return {
         ...state,
         inputValue: 0,
-        showingResult: false,
+        resultValue: state.resultValue + state.inputValue,
+        showingResult: true,
       };
     default:
       return state;
