7a78405a1433d025ffa2896e02db92b636d72625
asuforce <owata.sn@gmail.com>
2018-11-28 17:22:07 +0900

    Create basic routing

---
+++ react-router-v4-tutorial/src/App.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/friends" component={Friends} />
+    </div>
+  </BrowserRouter>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>フレンズに関するページです</p>
+  </div>
+);
+
+const Friends = () => (
+  <div>
+    <h2>Friends</h2>
+    <p>ここにフレンズのリストを書きます</p>
+  </div>
+);
+
+export default App;
