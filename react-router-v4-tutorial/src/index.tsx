adc9a5dd7137865fdd271935b230b7477ca3a182
asuforce <owata.sn@gmail.com>
2018-11-28 17:56:37 +0900

    Fix format

--- react-router-v4-tutorial/src/index.tsx
+++ react-router-v4-tutorial/src/index.tsx
@@ -1,5 +1,5 @@
-import App from "App";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import App from 'App';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
