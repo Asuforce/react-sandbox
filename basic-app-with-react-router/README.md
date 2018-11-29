2c1984eec38abf253c6957690affd27eff833e64
asuforce <owata.sn@gmail.com>
2018-11-29 13:35:16 +0900

    Create basic-app with react-router

---
+++ basic-app-with-react-router/README.md
@@ -0,0 +1,3 @@
+# basic-app-with-react-router
+
+ref: [Basics of React Router v4 – ITNEXT](https://itnext.io/basics-of-react-router-v4-336d274fd9e0)
---
+++ basic-app-with-react-router/src/index.tsx
@@ -0,0 +1,6 @@
+import App from 'App';
+import 'index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
